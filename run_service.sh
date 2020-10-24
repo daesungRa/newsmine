d930fa0846332a97baab65d0fb82c4c83a86dfae
daesungra <daesungra@gmail.com>
2020-10-24 17:52:57 +0900

    app: Add venv activate on windows

--- run_service.sh
+++ run_service.sh
@@ -6,6 +6,7 @@ cd "$PJT_DIR"
 service nginx reload
 service nginx restart
 
-. ./venv/bin/activate
+. ./venv/bin/activate  # in ubuntu
+# . ./venv/Scripts/activate  # in windows
 gunicorn --workers 1 --threads 8 --worker-class gevent --timeout 30 --name newsmine newsmine.server:app
 
